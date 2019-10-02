f564982bf6d6e5d93841aa8f6c83e5194a8f9e8a
Agustin Navcevich <agus-navce52@hotmail.com>
2019-10-02 20:08:43 -0300

    Added an API for information with the proper serverless config (#2)
    * added a new feature
    
    * added an api for information with a new table

--- commons/aws/dynamodb_helper.py
+++ commons/aws/dynamodb_helper.py
@@ -1,9 +1,11 @@
 import boto3
+import json
 import pandas as pd
 from commons.logger import logged
 from dynamodb_json import json_util as json_d
 
 client = boto3.client('dynamodb')
+
 
 def get_all_elements_from_table(table_name):
     paginator = client.get_paginator('scan')
@@ -28,3 +30,27 @@ def add_element_to_table(table_name, content):
         TableName=table_name,
         Item=json_d.dumps(content, as_dict=True)
     )
+
+
+@logged()
+def get_element_from_table(table_name, id):
+    Key = json.loads(json_d.dumps({'id':id}))
+    return client.get_item(TableName=table_name, Key=Key)
+
+@logged()
+def delete_element_form_table(table_name,id):
+    Key = json.loads(json_d.dumps({'id':id}))
+    return client.delete_item(TableName=table_name, Key=Key)
+
+@logged()
+def list_elements_from_table(table_name):
+    paginator = client.get_paginator('scan')
+    operation_parameters = {
+        'TableName': table_name
+    }
+
+    page_iterator = paginator.paginate(**operation_parameters)
+    items = []
+    for page in page_iterator:
+        items.extend(json_d.loads(page['Items']))
+    return items
